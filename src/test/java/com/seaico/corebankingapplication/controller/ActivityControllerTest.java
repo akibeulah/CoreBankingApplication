5c1adc5d4cdaf29bf45ca4130fe5cd9b19ce4773
Seaico Technologies <beulah.akindele@seaicotechnologies.com>
2024-02-07 17:26:01 +0100

    Setup tests (failing)

--- src/test/java/com/seaico/corebankingapplication/controller/ActivityControllerTest.java
+++ src/test/java/com/seaico/corebankingapplication/controller/ActivityControllerTest.java
@@ -1,4 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ActivityControllerTest {
-  
+package com.seaico.corebankingapplication.controller;
+
+import com.seaico.corebankingapplication.models.Activity;
+import com.seaico.corebankingapplication.repositories.ActivityRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@WebMvcTest(ActivityController.class)
+@WebMvcTest(ActivityController.class)
+@ComponentScan(basePackages = {"com.seaico.corebankingapplication.controllers"})
+public class ActivityControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Mock
+    private ActivityRepository activityRepository;
+    @InjectMocks
+    private ActivityController activityController;
+
+    @BeforeEach
+    public void setupMocks() {
+        Activity activity = new Activity();
+        activity.setId("f47ac13b-58cc-4372-a567-0e12b2c3d479");
+        activity.setUserId("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+        activity.setDescription("Description");
+
+//        when(activityRepository.findById(1L)).thenReturn(Optional.of(activity));
+    }
+
+    @Test
+    public void testFetchActivity() throws Exception {
+        mockMvc.perform(get("/activity/1"))
+                .andExpect(status().isOk())
+                .andExpect(System.out::println);
+    }
+
+    @Test
+    public void testFetchActivities() throws Exception {
+        mockMvc.perform(get("/activity"))
+                .andExpect(status().isOk())
+                .andExpect(System.out::println);
+    }
+
+    @Test
+    public void testFetchUserActivities() throws Exception {
+        mockMvc.perform(get("/activity/user"))
+                .andExpect(status().isOk())
+                .andExpect(System.out::println);
+    }
 }
--- src/test/java/com/seaico/corebankingapplication/repositories/ActivityRepositoryTest.java
+++ src/test/java/com/seaico/corebankingapplication/repositories/ActivityRepositoryTest.java
@@ -1,4 +1,43 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ActivityRepositoryTest {
-  
+package com.seaico.corebankingapplication.repositories;
+
+import com.seaico.corebankingapplication.models.Activity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class ActivityRepositoryTest {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @BeforeEach
+    public void setUpMocks() {
+        Activity activity1 = new Activity();
+        activity1.setId("f47ac13b-58cc-4372-a567-0e12b2c3d129");
+        activity1.setUserId("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+        activity1.setDescription("Description");
+
+        Activity activity2 = new Activity();
+        activity2.setId("f47ac13b-58cc-4372-a567-0e12b2c3d239");
+        activity2.setUserId("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+        activity2.setDescription("Description");
+
+        Activity activity3 = new Activity();
+        activity3.setId("f47ac13b-58cc-4372-a567-0e12b2c3d479");
+        activity3.setUserId("f47ac10b-58cc-4372-a567-0e02b2c3d478");
+        activity3.setDescription("Description");
+    }
+
+    @Test
+    public void testFindAllByUserId() {
+        List<Activity> activities = activityRepository.findAllByUserId("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+        assertThat(activities).hasSize(2);
+        assertThat(activities.get(0).getUserId()).isEqualTo("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+    }
 }
